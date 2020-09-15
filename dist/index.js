40186a36c74cdf7c5769f0eb9723e436784068d3
Tu Huynh <tu.huynh@linecorp.com>
2020-09-15 16:57:57 +0700

    Change target to ES6 and CommonJS

--- dist/index.js
+++ dist/index.js
@@ -1,21 +1,29 @@
-export function usePubSub() {
-    const events = Object.create(null);
-    function publish(event, ...data) {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.usePubSub = void 0;
+function usePubSub() {
+    var events = Object.create(null);
+    function publish(event) {
+        var data = [];
+        for (var _i = 1; _i < arguments.length; _i++) {
+            data[_i - 1] = arguments[_i];
+        }
         if (!events[event])
             return;
         events[event]
-            .forEach((callback) => callback(...data));
+            .forEach(function (callback) { return callback.apply(void 0, data); });
     }
     function subscribe(event, callback) {
         if (!events[event]) {
             events[event] = [];
         }
         events[event].push(callback);
-        const index = events[event].length - 1;
+        var index = events[event].length - 1;
         return function () {
             events[event].splice(index, 1);
         };
     }
-    return { publish, subscribe };
+    return { publish: publish, subscribe: subscribe };
 }
-export default usePubSub;
+exports.usePubSub = usePubSub;
+exports.default = usePubSub;
