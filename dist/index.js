d85daeed1c168332f844889a76a71216bbf6375f
Tu Huynh <tu.huynh@linecorp.com>
2020-09-16 15:31:08 +0700

    Update dist

--- dist/index.js
+++ dist/index.js
@@ -23,6 +23,23 @@ function usePubSub() {
             events[event].splice(index, 1);
         };
     }
+    function subscribeOnce(event, callback) {
+        if (!events[event]) {
+            events[event] = [];
+        }
+        var index = events[event].length;
+        function unsubscribe() {
+            events[event].splice(index, 1);
+        }
+        events[event].push(function () {
+            var data = [];
+            for (var _i = 0; _i < arguments.length; _i++) {
+                data[_i] = arguments[_i];
+            }
+            callback.apply(void 0, data);
+            unsubscribe();
+        });
+    }
     function clearAllSubscriptions(event) {
         if (!event)
             return;
@@ -33,7 +50,7 @@ function usePubSub() {
             return 0;
         return events[event].length;
     }
-    return { publish: publish, subscribe: subscribe, clearAllSubscriptions: clearAllSubscriptions, countSubscription: countSubscription };
+    return { publish: publish, subscribe: subscribe, subscribeOnce: subscribeOnce, clearAllSubscriptions: clearAllSubscriptions, countSubscription: countSubscription };
 }
 exports.usePubSub = usePubSub;
 exports.default = usePubSub;
