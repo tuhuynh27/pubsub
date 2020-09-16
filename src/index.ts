d85e0067c0b0957387c9f2095af3ad70a5925dd9
Tu Huynh <tu.huynh@linecorp.com>
2020-09-16 15:53:08 +0700

    Remove the optional todo

--- src/index.ts
+++ src/index.ts
@@ -44,7 +44,7 @@ export function usePubSub (): PubSub {
 export interface PubSub {
     publish (event: string, ...data: PubSubData[]): void
     subscribe (event: string, callback: PubSubHandler): PubSubUnsubscribe
-    subscribeOnce? (event: string, callback: PubSubHandler): void
+    subscribeOnce (event: string, callback: PubSubHandler): void
     subscribeAll? (callback: PubSubHandler): void // Todo
     clearAllSubscriptions (event: string): void
     countSubscription (event: string): number
